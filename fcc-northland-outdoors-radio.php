1d05c0ba70fe449c770a144fd6a2916fe3a7bb79
Braden Stevenson <braden.stevenson@forumcomm.com>
2016-05-24 15:46:35 -0500

    something wasn't commented out correctly

--- fcc-northland-outdoors-radio.php
+++ fcc-northland-outdoors-radio.php
@@ -156,22 +156,8 @@
 # ADMIN NOTICES
 --------------------------------------------------------------*/
 
-/**
-* Load radio page css outside admin pages
-*
-*@since 0.16.02.05
-*/
-function loadOnRadio (){
-  if ( ! is_admin() ) {
-    if ( is_page( 'radio' ) ) {
-      wp_enqueue_style( 'custom_css_norad', plugin_dir_url( __FILE__ ) . '/includes/css/fcc_norad.css' );
-      wp_enqueue_style( 'font_awesome', 'http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css' );
-      wp_enqueue_script( 'fcc_norad_js', plugin_dir_url( __FILE__ ) . '/includes/js/fcc_norad.js' );
-    }
-  }
-}
-add_action('wp_head', 'loadOnRadio');
- * Set Admin Notices
+
+ /* Set Admin Notices
  *
  * @author Josh Slebodnik <josh.slebodnik@forumcomm.com>
  * @since 0.16.02.08

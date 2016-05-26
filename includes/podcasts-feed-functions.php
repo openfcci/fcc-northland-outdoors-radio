254f7e28b5533256f8c5f4144d96124aec8cfc0d
ryan.veitch <ryan.r.veitch@gmail.com>
2016-05-26 15:01:59 -0500

    Cleaned PODCASTS FEED FUNCTIONS

--- includes/podcasts-feed-functions.php
+++ includes/podcasts-feed-functions.php
@@ -8,10 +8,10 @@
  * Load Podcasts Feed Template
  *
  * @since 0.16.02.03
+ * @version 1.16.05.26
  * @see includes/templates/podcasts-feed.php
  */
-
- function add_podcasts_feed(){
-	 # Load the feed template
-   load_template( NORADIO__PLUGIN_PATH . 'includes/templates/podcasts-feed.php' );
- }
+function add_podcasts_feed() {
+	# Load the feed template
+	load_template( NORADIO__PLUGIN_PATH . 'includes/templates/podcasts-feed.php' );
+}
